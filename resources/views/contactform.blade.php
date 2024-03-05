71dfa922b66ebecbfb7a77e0d374261ebfb35573
DYWerensteijn <70692046+DYWerensteijn@users.noreply.github.com>
2024-03-05 13:06:55 +0100

    Revert "Add cap to phone number and add error messages at form"

--- resources/views/contactform.blade.php
+++ resources/views/contactform.blade.php
@@ -1,7 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{-- translatable tag --}}
             {{ __('formulier') }}
         </h2>
     </x-slot>
@@ -10,42 +9,32 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div>
-                        @if($errors->any())
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{$error}}</li>
-
-                            @endforeach
-                        </ul>
-                        @endif
-                    </div>
                     <form method="post" action="{{route('contacten.store')}}">
                         @csrf
                         @method('post')
                         <div>
                             <label>Naam</label>
-                            <input type="text" name="name" placeholder="John Doe">
+                            <input type="text" name="name" placeholder="Naam">
                         </div>
                         <div>
                             <label>E-mail</label>
-                            <input type="text" name="e-mail" placeholder="voorbeeld@mailservice.com">
+                            <input type="text" name="e-mail" placeholder="E-mail">
                         </div>
                         <div>
                             <label>Telefoonnummer</label>
-                            <input type="tel" name="phone_number" maxlength="10" placeholder="0612345678">
+                            <input type="text" name="phone_number" placeholder="Telefoonnummer">
                         </div>
                         <div>
                             <label>Primair bedrijf</label>
-                            <input type="text" name="primary_company" placeholder="Bedrijf">
+                            <input type="text" name="primary_company" placeholder="Primair bedrijf">
                         </div>
                         <div>
                             <label>Plaatsnaam</label>
-                            <input type="text" name="city" placeholder="Voorbeeldstad">
+                            <input type="text" name="city" placeholder="Plaatsnaam">
                         </div>
                         <div>
                             <label>Eigenaar contact</label>
-                            <input type="text" name="contact_owner" placeholder="Jane Doe">
+                            <input type="text" name="contact_owner" placeholder="Eigenaar contact">
                         </div>
                         <div>
                             <label>Status lead</label>

557d58d1d3f544a8abb852d196a96b216d9b67e8
DYWerensteijn <werensteijn.d.y@outlook.com>
2024-03-18 20:29:26 +0100

    Cleaned up contacts routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ContactsController;
+use App\Http\Controllers\BedrijfController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,17 +24,31 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/contacten', function () {
-    return view('contacten');
-})->middleware(['auth', 'verified'])->name('contacten');
 
-Route::post('/contacten',[ContactsController::class, 'store'], function () {
-    return view('contacten');
-})->middleware(['auth', 'verified'])->name('contacten.store');
+Route::prefix('contacten')->middleware(['auth', 'verified'])->group(function() {
+    Route::get('', [ContactsController::class, 'index'])
+        ->name('contacten');
 
-Route::get('/bedrijven', function () {
-    return view('bedrijven');
-})->middleware(['auth', 'verified'])->name('bedrijven');
+    Route::post('',[ContactsController::class, 'store'])
+        ->name('contacten.store');
+
+    Route::get('/{contacts}/edit',[ContactsController::class, 'edit'])
+        ->name('contacten.edit');
+
+    Route::get('/{contacts}/destroy',[ContactsController::class, 'destroy'])
+        ->name('contacten.destroy');
+});
+
+Route::get('/bedrijven', [BedrijfController::class, 'indexB'])->middleware(['auth', 'verified'])->name('bedrijven');
+Route::post('/bedrijven',[BedrijfController::class, 'store'])->middleware(['auth', 'verified'])->name('bedrijven.store');
+
+// Route::get('/bedrijven', function () {
+//     return view('bedrijven');
+// })->middleware(['auth', 'verified'])->name('bedrijven');
+
+// Route::post('/bedrijven',[BedrijfController::class, 'store'], function () {
+//     return view('bedrijven');
+// })->middleware(['auth', 'verified'])->name('bedrijven.store');
 
 Route::get('/deals', function () {
     return view('deals');

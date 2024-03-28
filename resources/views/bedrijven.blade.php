709b1e0eb9399055f3cd6117cb41f9d0de0de59f
DYWerensteijn <werensteijn.d.y@outlook.com>
2024-03-28 10:47:51 +0100

    added translate tags

--- resources/views/bedrijven.blade.php
+++ resources/views/bedrijven.blade.php
@@ -16,7 +16,7 @@
 
                     <div id="formContainer" class="hidden bg-sand">
                         <button id="closeFormBtn" type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                            <span class="sr-only">Close menu</span>
+                            <span class="sr-only">{{__('Close menu' )}}</span>
                             <!-- Heroicon name: outline/x -->
                             <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -26,35 +26,35 @@
                             @csrf
                             @method('post')
                             <div class="w-full md:w-1/2 flex-shrink-0 mt-6">
-                                <label for="naam" class="block">Naam onderneming</label>
+                                <label for="naam" class="block">{{__('Naam onderneming')}}</label>
                                 <input type="text" id="naam" name="naam" placeholder="Bedrijf BV" class="form-input">
                                 {{--Class "form input" is a premade tailwind CSS styling, which I can repeat among the other input tags. I did this so the code looks cleaner. You can find the full styling in app.css--}}
                             </div>
                             <div class="w-full md:w-1/2 flex-shrink-0">
-                                <label for="bedrijfseigenaar" class="block">Bedrijfseigenaar</label>
+                                <label for="bedrijfseigenaar" class="block">{{__('Bedrijfseigenaar')}}</label>
                                 <input type="text" id="bedrijfseigenaar" name="bedrijfseigenaar" placeholder="Naamomi Namerland" class="form-input">
                             </div>
                             <div class="w-full md:w-1/2 flex-shrink-0">
-                                <label for="straat" class="block">Straat</label>
+                                <label for="straat" class="block">{{__('Straat')}}</label>
                                 <input type="text" id="straat" name="straat" placeholder="Voorbeeldstraat" class="form-input">
                             </div>
                             <div class="w-full md:w-1/2 flex-shrink-0">
-                                <label for="huisnummer" class="block">Huisnummer</label>
+                                <label for="huisnummer" class="block">{{__('Huisnummer')}}</label>
                                 <input type="text" id="huisnummer" name="huisnummer" placeholder="00" class="form-input">
                             </div>
                             <div class="w-full md:w-1/2 flex-shrink-0">
-                                <label for="postcode" class="block">Postcode</label>
+                                <label for="postcode" class="block">{{__('Postcode')}}</label>
                                 <input type="text" id="postcode" name="postcode" placeholder="1122AB" class="form-input">
                             </div>
                             <div class="w-full md:w-1/2 flex-shrink-0">
-                                <label for="branche" class="block">Branche</label>
+                                <label for="branche" class="block">{{__('Branche')}}</label>
                                 <input type="text" id="branche" name="branche" placeholder="Marketing" class="form-input">
                             </div>
                             <div class="w-full mt-4">
-                                <button type="submit" class="form-styledbutton">Sla Bedrijf Op</button>
+                                <button type="submit" class="form-styledbutton">{{__('Sla Bedrijf Op')}}</button>
                             </div>
                             <div class="w-full">
-                                <button type="button" class="form-styledbutton">Annuleren</button>
+                                <button type="button" class="form-styledbutton">{{__('Annuleren')}}</button>
                             </div>
                         </form>
                         <div>
@@ -71,13 +71,13 @@
 
                     <table>
                         <tr>
-                            <th>Naam onderneming</th>
-                            <th>Bedrijfseigenaar</th>
-                            <th>Adres</th>
-                            <th>Branche</th>
-                            <th>Datum aanmaak</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
+                            <th>{{__('Naam onderneming')}}</th>
+                            <th>{{__('Bedrijfseigenaar')}}</th>
+                            <th>{{__('Adres')}}</th>
+                            <th>{{__('Branche')}}</th>
+                            <th>{{__('Datum aanmaak')}}</th>
+                            <th>{{__('Edit')}}</th>
+                            <th>{{__('Delete')}}</th>
                         </tr>
                             {{-- Posts records from tabble --}}
                         @foreach($bedrijven as $bedrijf)
@@ -88,7 +88,7 @@
                             <td class="text-transform: capitalize">{{ $bedrijf->branche }}</td>
                             <td>{{ $bedrijf->datum_aanmaak }}</td>
                             <td>
-                                <a href="{{route('bedrijven.edit', ['company' => $bedrijf])}}">Edit</a>
+                                <a href="{{route('bedrijven.edit', ['company' => $bedrijf])}}">{{__('Edit')}}</a>
                             </td>
                             <td>
                                 <form method="post" action="{{ route('bedrijven.destroy', ['company' => $bedrijf->id]) }}">
--- resources/views/contacten/edit.blade.php
+++ resources/views/contacten/edit.blade.php
@@ -5,57 +5,56 @@
         </h2>
     </x-slot>
 
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
-<form id="myForm" action="{{ route('contacten.editHandler', ['contacts' => $contact->id]) }}" method="POST">
-    @csrf
-    @method('post')
-        <div>
-            <label>Naam</label>
-            <input value="{{$contact->name}}" type="text" name="name" placeholder="John Doe">
-        </div>
-        <div>
-            <label>E-mail</label>
-            <input value="{{$contact->email}}" type="text" name="email" placeholder="voorbeeld@mailservice.com">
-        </div>
-        <div>
-            <label>Telefoonnummer</label>
-            <input value="{{$contact->phone_number}}" type="tel" name="phone_number" maxlength="10" placeholder="0612345678">
-        </div>
-        <div>
-            <label>Primair bedrijf</label>
-            <input value="{{$contact->primary_company}}" type="text" name="primary_company" placeholder="Bedrijf">
-        </div>
-        <div>
-            <label>Plaatsnaam</label>
-            <input value="{{$contact->city}}" type="text" name="city" placeholder="Voorbeeldstad">
-        </div>
-        <div>
-            <label>Eigenaar contact</label>
-            <input value="{{$contact->contact_owner}}" type="text" name="contact_owner" placeholder="Jane Doe">
-        </div>
-        <div>
-            <label>Status lead</label>
-            <select name="lead_status">
-                <option value="nieuw">Nieuw</option>
-                <option value="openen">Openen</option>
-                <option value="in behandeling">In behandeling</option>
-                <option value="deal openen">Deal openen</option>
-                <option value="ongekwalificeerd">Ongekwalificeerd</option>
-                <option value="geprobeerd contact op te nemen met">Geprobeerd contact op te nemen met</option>
-                <option value="verbonden">Verbonden</option>
-                <option value="slechte timing">Slechte timing</option>
-            </select>
-         </div>
-        <input class="p-3 rounded bg-green-400" type="submit" value="Verander contact informatie">
-</form>
+                    <form id="myForm" action="{{ route('contacten.editHandler', ['contacts' => $contact->id]) }}" method="POST" class="w-full md:w-1/2 lg:w-1/4 mx-auto">
+                        @csrf
+                        @method('post')
+                        <div class="mb-4">
+                            <label class="block">{{__('Naam') }}</label>
+                            <input value="{{$contact->name}}" type="text" name="name" placeholder="John Doe" class="w-full px-3 py-2 border border-gray-300 rounded-md">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block">{{__('E-mail') }}</label>
+                            <input value="{{$contact->email}}" type="text" name="email" placeholder="voorbeeld@mailservice.com" class="w-full px-3 py-2 border border-gray-300 rounded-md">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block">{{__('Telefoonnummer') }}</label>
+                            <input value="0{{$contact->phone_number}}" type="tel" name="phone_number" maxlength="10" placeholder="0612345678" class="w-full px-3 py-2 border border-gray-300 rounded-md">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block">{{__('Primair bedrijf') }}</label>
+                            <input value="{{$contact->primary_company}}" type="text" name="primary_company" placeholder="Bedrijf" class="w-full px-3 py-2 border border-gray-300 rounded-md">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block">{{__('Plaatsnaam') }}</label>
+                            <input value="{{$contact->city}}" type="text" name="city" placeholder="Voorbeeldstad" class="w-full px-3 py-2 border border-gray-300 rounded-md">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block">{{__('Eigenaar contact') }}</label>
+                            <input value="{{$contact->contact_owner}}" type="text" name="contact_owner" placeholder="Jane Doe" class="w-full px-3 py-2 border border-gray-300 rounded-md">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block">{{__('Status lead') }}</label>
+                            <select name="lead_status" class="w-full px-3 py-2 border border-gray-300 rounded-md">
+                                <option value="nieuw">{{__('Nieuw') }}</option>
+                                <option value="openen">{{__('Openen') }}</option>
+                                <option value="in behandeling">{{__('In behandeling') }}</option>
+                                <option value="deal openen">{{__('Deal openen<')}}/option>
+                                <option value="ongekwalificeerd">{{__('Ongekwalificeerd') }}</option>
+                                <option value="geprobeerd contact op te nemen met">{{__('Geprobeerd contact op te nemen met') }}</option>
+                                <option value="verbonden">{{__('Verbonden') }}</option>
+                                <option value="slechte timing">{{__('Slechte timing') }}</option>
+                            </select>
+                        </div>
+                        <input type="submit" value="Verander contact informatie" class=" bg-green-400 text-white px-4 py-2 rounded-md hover:bg-green-500 cursor-pointer">
+                    </form>
 
-</div>
-</div>
-</div>
-</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
